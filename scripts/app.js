488300d987a3b1590bc2ac38e02dac03c482be52
Ashwin <ashwin.sriv13@gmail.com>
2016-10-16 15:55:36 +0530

    Added ng-model for real time updates using the scoping functionality

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,11 @@
 'use strict';
 
 	angular.module('firstAngularJsApp', [])
-	.controller ('firstController', function () {
+	.controller ('firstController', function ($scope) {
+		$scope.name = "Lorem ipsum";
+		$scope.sayHello = function () {
+			return "Hello Guest";
+		}
 
 
 
