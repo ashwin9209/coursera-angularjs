4cae6b1ac4c73791105675f7c10a518a2fa64ca3
Ashwin <ashwin.sriv13@gmail.com>
2016-11-14 09:34:31 +0530

    Added inject service to prevent code breakage during JS minification

--- dependency-injection/scripts/app.js
+++ dependency-injection/scripts/app.js
@@ -4,6 +4,8 @@
 angular.module('DIApp', [])
 .controller('DIController', DIController);
 
+DIController.$inject = ['$scope','$filter'];
+
 function DIController($scope, $filter) {
 	$scope.name = 'LifeCycle';
 
